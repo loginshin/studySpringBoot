c9fbe96b5df19ff0de751ec0d9202d5e4754b9af
loginshin <95666408+loginshin@users.noreply.github.com>
2023-07-20 17:27:29 +0900

    firstcommit

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,48 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!!!!!!");
+
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-String")
+    @ResponseBody //리스폰 바디를 어노테이션 사용 시 → 뷰 리졸버를 사용하지 않음
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
